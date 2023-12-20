ea34b24738fa67573bce50f85cd620705882e3b4
sonms <smsms5676@naver.com>
2023-12-20 22:07:09 +0900

    Feature/Compose 투두리스트 만들기 - 11. 세팅화면 만들기 준비

---
+++ app/src/main/java/com/example/mycomposetodolist/component/SettingScreen.kt
@@ -0,0 +1,10 @@
+package com.example.mycomposetodolist.component
+
+import androidx.compose.runtime.Composable
+
+//이제는 preferences screen xml 대신 protoDataStore을 사용
+@Composable
+fun SettingScreen() {
+
+}
+
